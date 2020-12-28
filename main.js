97382a53ff17d62ab3d2b93f7411cce662691085
tikarya <tea.4.me0@gmail.com>
2020-12-28 13:44:14 +0100

    add fos part 1 selects

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ let grade_count = 0;
 //grades = [[12,11,10,5,8,11,12,13,15,,5,12],[8,11,12,11,5,14,7,8,9,,11],[2,6,4,8]];
 
 // The different objects for Fachrichtungen (Fr)
-const tec = new fachrichtung("Technik",[["Ph","Physik"],["Ch","Chemie"],["M+","Mathe Additum"],["T","Technik"]]);
-const health = new fachrichtung("Gesundheit",[["GHW","Gesundheitswissenschaften"],["Bio","Biologie"],["Ch","Chemie"],["KoInt","Kommunikation und Interaktion"]]);
-const eco = new fachrichtung("Wirtschaft und Verwaltung",[["BWR","Betriebswirtschaftslehre mit Rechnungswesen"],["R","Rechtslehre"],["VWL","Volkswirtschaftslehre"],["NW","Naturwissenschaften"],["IT","Informatik"]],true);
-const social = new fachrichtung("Sozialwesen",[["PsPa","Pädagogik/Psychologie"],["Bio","Biologie"],["Ch","Chemie"],["SWR","Sozialwirtschaft und Recht"],["Soz","Soziologie"]],true);
+const tec = new fachrichtung("Technik",[["Ph","Physik"],["T","Technik"],["Ch","Chemie"],["M+","Mathe Additum"]]);
+const health = new fachrichtung("Gesundheit",[["GHW","Gesundheitswissenschaften"],["KoInt","Kommunikation und Interaktion"],["Ch","Chemie"],["Bio","Biologie"]]);
+const eco = new fachrichtung("Wirtschaft und Verwaltung",[["BWR","Betriebswirtschaftslehre mit Rechnungswesen"],["VWL","Volkswirtschaftslehre"],["NW","Naturwissenschaften"],["IT","Informatik"],["R","Rechtslehre"]],true);
+const social = new fachrichtung("Sozialwesen",[["PsPa","Pädagogik/Psychologie"],["SWR","Sozialwirtschaft und Recht"],["Bio","Biologie"],["Soz","Soziologie"],["Ch","Chemie"]],true);
 
 // Array for output with all Fr connected to select values on index.html
 const frs = [tec,health,eco,social];
@@ -33,10 +33,18 @@ const frs = [tec,health,eco,social];
 //Selecting and displaying the Fr and saving selection in Local Storage
 function select_fr(fr){
     localStorage.setItem("fr",JSON.stringify(fr));
+    const nodes = document.getElementsByClassName("diff");
     if (fr.difmath) {
-        document.getElementById("rowPF4").style.display = "table-row";
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            node.classList.add("show"); 
+        }
+        
     }else{
-        document.getElementById("rowPF4").style.display = "none";
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            node.className = "diff"; 
+        }
     }
     for (let i = 0; i < fr.pf.length; i++) {
       const element = fr.pf[i];
@@ -45,18 +53,18 @@ function select_fr(fr){
   }
 }
 // basically same thing for Fr but with WPF
-function select_wpf(){
+function select_wpf(id){
     // get node for table
-    const selectedWPF = document.getElementById("selectWPF");
+    const selectedWPF = document.getElementById("selectWPF"+id);
     // get node contents
     const wpfname = selectedWPF.options[selectedWPF.selectedIndex].text;
     const vals = selectedWPF.options[selectedWPF.selectedIndex].value.split(",");
 
     // make WPF object and save it to local Storage
     const wpf = new wahlpflichtfach(wpfname,vals[0],vals[1]);
-    localStorage.setItem("wpf",JSON.stringify(wpf));
+    localStorage.setItem("wpf"+id,JSON.stringify(wpf));
     //
-    const node = document.getElementById("wpf");
+    const node = document.getElementById("wpf"+id);
     node.innerText = vals[0];
     node.title = wpfname;
 
