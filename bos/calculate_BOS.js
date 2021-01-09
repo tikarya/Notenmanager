2fe5bd07bb3ab6b8b370dff254fcedc1f6d75c0e
tikarya <tea.4.me0@gmail.com>
2021-01-09 16:27:44 +0100

    average and calc merge

--- bos/calculate_BOS.js
+++ bos/calculate_BOS.js
@@ -39,75 +39,11 @@ function getAllHY(){
 function getOppotunities(){
     return 17;
 }
-
-//calculate the average
-function calculate(){
-    let all = 0;
-    for (let i = 0; i < grades.array.length-1; i++) {
-        const array = grades.array[i];
-        for (let j = 0; j < array.length; j++) { 
-
-            if(array[j] != (null||undefined)){
-                let element = array[j];
-                if (!(grades.crossed.some((el)=> el === element))){
-                    
-                    if(element[1] == 2){
-                        if (grades.array[i+1][j]!= (null||undefined)) {
-                            all += Math.round((((element[0]*2)+ grades.array[i+1][j][0])/3))*2;
-                        }else{
-                            // APs have to be counted twice
-                            all += element[0] * 2;
-                        }
-                    }else{
-                        all += element[0];
-                    }
-
-                }
-            }
-        }
-    }
-
-    let erg = (17/3-5*all/375).toFixed(2);
-    if (erg< 1) {
-        erg = "1.00";
-    }
-    document.getElementById("erg").innerText ="Notendurchschnitt: "+ (erg);
-        
+function getAPCount(){
+    return 2;
 }
-function set_average(subject){
-
-    grades.average[0][subject] = 0;
-    grades.average[1][subject] = 0;
-
-    for (let i = 0; i < 3; i++) {
-        const element = grades.array[i][subject];
-        if (element != (undefined||null)) {
-            if (!(grades.crossed.some((el)=> el === element))){  
-                if(i == 2){
-                    if (grades.array[i+1][subject]!= (null||undefined)) {
-                        grades.average[1][subject] += Math.round((((element[0]*2+grades.array[i+1][subject][0])/3))*2);
-                    }else{
-                        // APs have to be counted twice
-                        grades.average[1][subject] += grades.array[i][subject][0]*2;
-                    }
-                    grades.average[0][subject] +=2;
-
-                }else{
-                    grades.average[1][subject] += grades.array[i][subject][0];
-                    grades.average[0][subject]++;
-                }
-
-            }
-               
-        }
-    }
-    for (let i = 0; i < grades.average[0].length; i++) {
-        const element = grades.average[0][i];
-        if (element != (undefined||null) && grades.average[1][i]!= (undefined||null)) {
-            grades.average[2][i] = (grades.average[1][i]/element).toFixed(2);
-            document.getElementById(i).innerHTML = grades.average[2][i];
-        }
-    }
+function getPointsPossible(){
+    return 375;
 }
 
 function evaluate(){
--- fos/calculate_FOS.js
+++ fos/calculate_FOS.js
@@ -1,10 +1,10 @@
 "use strict";
 
 function gradeObject(){
-    this.array      = [new Array(13),new Array(13),new Array(14),new Array(12),new Array(4),new Array(4)];
+    this.array      = [new Array(14),new Array(14),new Array(15),new Array(13),new Array(4),new Array(4)];
     this.count      = 0;
     this.crossed    = [];
-    this.average    = new Array(14);
+    this.average    = [new Array(15),new Array(15),new Array(15)];
     this.less4HY    = 0;
     this.less3AP    = 0;
 }
@@ -16,7 +16,7 @@ function checkCounting(subject,type){
         && grades.array[type][subject]== undefined
         &&subject != grades.array[1].length-1 ){                                  // disregarding already counted grades7
 
-        if ((subject == 10 && localObject.WPF1.valid == 0 || subject == 11 && localObject.WPF2.valid == 0)) {
+        if ((subject == 11 && localObject.WPF1.valid == 0 || subject == 12 && localObject.WPF2.valid == 0)) {
             
         }else{
             //Counts input grades of HYs for crossing them out
@@ -34,12 +34,12 @@ function getAllHY(){
     array[2].length -= 1;   
 
     if (localObject.WPF2.valid == 0) {
-        array[2].splice(array[2].lastIndexOf(grades.array[2][11]),1);
-        array[3].splice(array[2].lastIndexOf(grades.array[3][11]),1);
+        array[2].splice(array[2].lastIndexOf(grades.array[2][12]),1);
+        array[3].splice(array[2].lastIndexOf(grades.array[3][12]),1);
     }
     if (localObject.WPF1.valid == 0) {
-        array[2].splice(array[2].lastIndexOf(grades.array[2][10]),1);
-        array[3].splice(array[2].lastIndexOf(grades.array[3][10]),1);
+        array[2].splice(array[2].lastIndexOf(grades.array[2][11]),1);
+        array[3].splice(array[2].lastIndexOf(grades.array[3][11]),1);
     }
 
     let allhy = array[0];
@@ -48,39 +48,15 @@ function getAllHY(){
     }
     return allhy;
 }
+
 function getOppotunities(){
     return 25;
 }
 
-function calculate(){
-    let all = 0;
-    for (let i = 0; i < grades.array.length-1; i++) {
-        const array = grades.array[i];
-        for (let j = 0; j < array.length; j++) { 
+function getAPCount(){
+    return 3;
+}
 
-            if(array[j] != (null||undefined)){
-                let element = array[j];
-                // APs have to be counted twice
-                if (!(grades.crossed.some((el)=> el === element))){
-                    if(element[1] == 4){
-                        if (grades.array[i+1][j]!= (null||undefined)) {
-                            all += Math.round((((element[0]*2)+ grades.array[i+1][j][0])/3))*3;
-                        }else{
-                            all += element[0] * 3;
-                        }
-                    }else{
-                        all += element[0];
-                    }
-                    
-                   
-                }
-            }
-        }
-    }
-    let erg = (17/3-5*all/600).toFixed(2);
-    if (erg< 1) {
-        erg = "1.00";
-    }
-    document.getElementById("erg").innerText ="Notendurchschnitt: "+ (erg);
-        
+function getPointsPossible(){
+    return 600;
 }
